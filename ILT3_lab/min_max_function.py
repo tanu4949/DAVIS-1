1810b31c9674ef0ac2c8600c9dd70593f741e352
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-23 01:41:42 +0530

    Add files via upload
    python program for Using max() and min() functions display the maximum and minimum of 5 random
    numbers.

---
+++ ILT3_lab/min_max_function.py
@@ -0,0 +1,10 @@
+#Using max() and min() functions display the maximum and minimum of 5 random numbers.
+import random
+for random_number in range(0,5):
+    random_number = random.randint(1, 20)
+    lis1=[random_number]
+    print(lis1)
+    
+print("the maximum of random numbers is",max(lis1))
+print("the minimum of random numbers is",min(lis1))
+
