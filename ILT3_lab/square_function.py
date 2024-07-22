e3f5efd201074add6847853082b83ab0d0de527a
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-23 01:42:36 +0530

    Add files via upload
    python program for Declare a square() function with one parameter .Then call the function and pass
    one number and display the square of that number

---
+++ ILT3_lab/square_function.py
@@ -0,0 +1,7 @@
+##Declare a square() function with one parameter.Then call the function and pass one number and display the square of that number 
+def square(a):
+    d=(a*a)
+    print("the numbers is",a)
+    print("the square of number is",d)
+
+(square(4))
