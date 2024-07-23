a2bf6324ab7bc1646758c7285084a4c0165fd7d9
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-23 12:35:45 +0530

    Add files via upload
    #creating the class and its objects in python

---
+++ oops_exceptionHandling/creation_class.py
@@ -0,0 +1,11 @@
+#ceating the class and its objects
+class student:
+    name="xyz"
+    age=0
+    def study(self):
+        print("study 3 hour")
+        self.sleep()
+    def sleep(self):
+        print("sleep 1 hout")
+obj=student()
+obj.study()
