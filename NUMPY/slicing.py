53d98d6e77dc013a0101d43de9b4f867665a6fbf
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-25 19:37:38 +0530

    create slicing.py

--- NUMPY/slicing.py
+++ NUMPY/slicing.py
@@ -7,3 +7,15 @@ print(sarr)
 sarr[2]=100
 print(arr)
 print(sarr)
+
+#slicing in array and type
+import numpy as n
+arr=n.array([1,20,5,9,3,4,8,5,6,2,25,2,2265,3,33])
+slicing=arr[4:9]
+print("new slicing",slicing)
+print("new array",arr)
+print(type(slicing))
+print(type(arr))
+slicing[:]=0
+print("new slicing",slicing)
+print("new array",arr)
