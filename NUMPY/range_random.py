cbff6c0be8a719a007c9430dcda6812f6f5fbfcd
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-25 19:33:42 +0530

    Create range_random.py

---
+++ NUMPY/range_random.py
@@ -0,0 +1,14 @@
+#print random number between range
+import numpy as np
+rand= np.random.rand(4) #any 4 number
+print(rand)
+
+#print random number between range
+import numpy as np
+rand= np.random.randint(0,100,5) #5 number from range 0 to 100
+print(rand)
+
+#print random number between range
+import numpy as np
+rand= np.random.rand(2,8) #row and column
+print(rand)
