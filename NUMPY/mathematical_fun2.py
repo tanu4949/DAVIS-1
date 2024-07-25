0124622c7c57142d92b06e6bd70210749b655e31
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-25 19:39:51 +0530

    Create mathematical_fun2.py
    python program for performing mathematical functions on the array

---
+++ NUMPY/mathematical_fun2.py
@@ -0,0 +1,10 @@
+#python program for performing mathematical functions on the array
+import numpy as n
+arr=n.array([10,20,30,40,6,2])
+print("the minimum of array is",n.min(arr))
+print("the maximum of array is",n.max(arr))
+print("the index of minimum of array is",n.argmin(arr))
+print("the index of maximum of array is",n.argmax(arr))
+print("the square root of array",n.sqrt(arr))
+print("the sin value of array values",n.sin(arr))
+print("the cos value of array values",n.cos(arr))
