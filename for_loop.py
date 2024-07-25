cd220b8ceab0f4c216d7511fd7744955bf624a24
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-25 19:52:15 +0530

    Delete for_loop.py

--- for_loop.py
+++
@@ -1,8 +0,0 @@
-count=0
-print("the numbers between 100 to 1000 which is even and divisible by 3")
-for i in range(100,1000):
-    if(i%2==0 and i%3==0):
-        print(i,end=",")
-        count=count=1
-print("\n--------------------------------------")
-print("the count is",count)
