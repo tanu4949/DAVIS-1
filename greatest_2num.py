3de5f99821b24d0277403fd08dc78f8efdfb4a29
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-25 20:36:19 +0530

    Delete greatest_2num.py

--- greatest_2num.py
+++
@@ -1,8 +0,0 @@
-num1=int(input("Enter the first number"))
-num2=int(input("Enter the second number"))
-if(num1==num2):
-    print("they are equal")
-elif (num1>num2):
-  print(num1,"is greater")
-else:
-  print(num2,"is greater")
