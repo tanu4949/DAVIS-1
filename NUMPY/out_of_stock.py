83f8cb05fcc46d4dccaec8cccc7e2a333a484f73
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-29 15:04:16 +0530

    Delete NUMPY/out_of_stock.py

--- NUMPY/out_of_stock.py
+++
@@ -1,5 +0,0 @@
-#determine which product  in a store is out of stock(quantity=0)
-import numpy as np
-product=np.array([10,0,0,20,0])
-d= product[product==0]
-print("the product which are out of stock are",d)
