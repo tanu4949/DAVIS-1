399543cb829714708f4decc253d84fedef8f26e0
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-29 16:33:22 +0530

    Create Q3purchase.py

---
+++ NUMPY/ILT-2LAB/Q3purchase.py
@@ -0,0 +1,6 @@
+#Suppose you have a dataset containing customer data, and you want to split this data
+#into two groups: one group for customers who made a purchase in the last 30 days and
+#another group for customers who haven't made a purchase in the last 30 days.
+
+
+
