2fbd740205db69146e2673426c6602bf9a997857
tanu4949 <123127722+tanu4949@users.noreply.github.com>
2024-07-29 15:05:42 +0530

    Delete NUMPY/total_cost.py

--- NUMPY/total_cost.py
+++
@@ -1,7 +0,0 @@
-#calculate the total cost of items in a shopping cart,considering the price per item
-
-import numpy as np
-quantity=np.array([2,3,4,1])
-price_item=np.array([10.0,5.0,8.0,12.0])
-c = np.multiply(quantity,price_item)
-print("total cost of items is:",c)
